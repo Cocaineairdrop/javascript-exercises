6c004a3331c336be5b680992dd5c9e09056d7852
Cocaineairdrop <madonna.s.murphy@gmail.com>
2025-03-31 15:30:03 -0700

    All tests passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -7,4 +7,9 @@ module.exports = removeFromArray;
 
 /*The includes() method of Array instances determines 
 whether an array includes a certain value among its 
-entries, returning true or false as appropriate. */
+entries, returning true or false as appropriate. 
+
+The filter() method of Array instances creates a 
+shallow copy of a portion of a given array, filtered 
+down to just the elements from the given array that 
+pass the test implemented by the provided function.*/
