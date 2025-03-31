99d9a0643d588bf583c09dadbb394f7feeda1cd7
Cocaineairdrop <madonna.s.murphy@gmail.com>
2025-03-31 11:42:34 -0700

    Using for loop and += to add strings together by num of times given, also added Error message if num is negative

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num >= 0) {
+        let str = "";
+        for (let i = 0; i < num; i++) {
+            str += string;
+        }
+        return str;
+    } else {
+        return "ERROR";
+    }
 };
-
 // Do not edit below this line
 module.exports = repeatString;
+
+/* The += operator can also be used for string 
+concatenation. If str = "Hello", then str += " World" 
+is equivalent to str = str + " World", resulting in str 
+being assigned the value "Hello World".*/
