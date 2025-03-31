a65820b523e01305d08ee729d1922cd5138c5734
Cocaineairdrop <madonna.s.murphy@gmail.com>
2025-03-31 16:19:48 -0700

    Test 3 passed where large number came first to sum

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,15 +2,23 @@ const sumAll = function(start, end) {
     let array = [start, end];
     let x = start;
     let y = end;
-    
-    while (x < (y-1)) {
-      array.push(x + 1);
-      x++;
+
+    if (x < y) {
+        while (x < (y-1)) {
+            array.push(x + 1);
+            x++;
+        } 
+    } else {
+        while (y < (x-1)) {
+            array.push(y + 1);
+            y++;
+        }
     }
+   
     const arrayToSum = array;
     
     const sum = eval(arrayToSum.join('+'));
-    
+
     return sum;
 };
 
