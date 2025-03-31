7516a3b14c9d82cb75d1722a645e9547313ebcd6
Cocaineairdrop <madonna.s.murphy@gmail.com>
2025-03-31 11:51:11 -0700

    Passed all reverse string tests using split(), reverse(), and Join()

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    var splitWord = word.split("");
+    var reverseArray = splitWord.reverse();
+    var joinArray = reverseArray.join("");
 
+    return joinArray;
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
+/* split() - splits the string into letters into array
+i.e. hello = ["h", "e", "l", "l", "o"]
+
+reverse() - reverses the array elements
+
+join() - join all elements in array*/
