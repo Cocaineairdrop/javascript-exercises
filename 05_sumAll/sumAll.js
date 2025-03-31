f05293fb4791ddadedae28037b63e08c1001236e
Cocaineairdrop <madonna.s.murphy@gmail.com>
2025-03-31 16:14:49 -0700

    passed 1st test using while loops and arrays to sum items together

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let array = [start, end];
+    let x = start;
+    let y = end;
+    
+    while (x < (y-1)) {
+      array.push(x + 1);
+      x++;
+    }
+    const arrayToSum = array;
+    
+    const sum = eval(arrayToSum.join('+'));
+    
+    return sum;
 };
 
 // Do not edit below this line
